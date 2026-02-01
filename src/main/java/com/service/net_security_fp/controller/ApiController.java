013ba126c2240bb63cf74de565f8ed09cd0b6765
Viktor Kulakov <plumking.lol@gmail.com>
2026-02-01 23:15:22 +0300

    Grafana & Prometheus metrics added.

--- src/main/java/com/service/net_security_fp/controller/ApiController.java
+++ src/main/java/com/service/net_security_fp/controller/ApiController.java
@@ -1,5 +1,7 @@
 package com.service.net_security_fp.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -8,13 +10,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApiController {
+
+    private final Counter publicPageCounter;
+    private final Counter privatePageCounter;
+
+    public ApiController(MeterRegistry registry) {
+        this.publicPageCounter = Counter.builder("app.pages.views")
+                .tag("type", "public")
+                .description("Количество просмотров главной страницы")
+                .register(registry);
+        this.privatePageCounter = Counter.builder("app.pages.views")
+                .tag("type", "private")
+                .description("Количество успешных входов в приватную зону")
+                .register(registry);
+    }
+
     @GetMapping("/")
     public String publicPage() {
-        return "index"; // index.html в папке resources/templates
+        publicPageCounter.increment();
+        return "index";
     }
 
     @GetMapping("/private")
     public String privatePage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        privatePageCounter.increment();
         if (principal != null) {
             model.addAttribute("name", principal.getAttribute("name"));
         }
